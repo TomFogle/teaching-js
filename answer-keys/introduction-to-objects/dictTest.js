2af24fbb9652114b8d6fc2f7ec9832c34f0a2989
Tom Fogle <tomtfogle@gmail.com>
2020-07-08 20:20:37 -0400

    removed duplicate file

--- answer-keys/introduction-to-objects/dictTest.js
+++ answer-keys/introduction-to-objects/dictTest.js
@@ -22,3 +22,21 @@ You should get:
 
 // Now try this line
 console.log(jobRoles["Alice Garfunkel"]);
+
+
+// You could also do this with key names that don't necessarily need the ""
+console.log(jobRoles["Leo"]);
+
+/*
+Think of the "Alice Garfunkel" key name as a vegan, the Leo key name as a meat eater,
+the [""] as a vegan diet, and .name as meat.
+
+While vegans can only eat plants,
+meat eaters can eat both meat and plants.
+
+While "Alice Garfunkel" can only be accessed using the [""] notation,
+Leo can be accessed using both the . notation and the [""] notation.
+*/
+
+
+
